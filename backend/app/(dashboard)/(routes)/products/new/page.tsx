5c90003a6dbe0306e5590037b34f7d2690a8da32
Vaibhav Singh <vaibhavpratapsingh22@gmail.com>
2024-01-31 17:06:40 +0530

    new product images

--- backend/app/(dashboard)/(routes)/products/new/page.tsx
+++ backend/app/(dashboard)/(routes)/products/new/page.tsx
@@ -1,29 +1,30 @@
 "use client";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const NewProduct = () => {
+  const formRef: React.MutableRefObject<HTMLFormElement | null> = useRef(null);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
-  const [image, setImage] = useState<String[]>([]);
+  const [uploadedImages, setUploadedImage] = useState<String[]>([]);
   const clearForm = () => {
     setName("");
     setPrice("");
     setDescription("");
-    setImage([]);
+    setUploadedImage([]);
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = { name, price, description, image };
-    console.log(data);
+    const data = { name, price, description, uploadedImages };
+
     try {
       const response = await axios.post("/api/product", data);
-      console.log(response);
       if (response.status === 201) {
         toast.success("Product saved successfully !");
         clearForm();
+        formRef?.current?.reset();
       } else {
         toast.error(response.data.message);
       }
@@ -37,7 +38,6 @@ const NewProduct = () => {
   };
   const handleUpload = (ev: React.ChangeEvent<HTMLInputElement>) => {
     const files: FileList | null = ev.target.files;
-    console.log(files, files?.length);
     if (files && files?.length > 0) {
       const imagesArray: string[] = [];
 
@@ -49,13 +49,17 @@ const NewProduct = () => {
         };
         reader.readAsDataURL(currentFile);
       }
-      setImage(imagesArray);
+      setUploadedImage(imagesArray);
     }
   };
   return (
     <>
       <h2>Add new product </h2>
-      <form onSubmit={handleSubmit} className="mt-8 grid grid-cols-6 gap-6">
+      <form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="mt-8 grid grid-cols-6 gap-6"
+      >
         <div className="col-span-6 sm:col-span-3">
           <label className="block text-sm font-medium text-gray-700">
             Product Name
@@ -99,6 +103,7 @@ const NewProduct = () => {
             onChange={(ev) => handleUpload(ev)}
             id="ProductImage"
             name="Product Image"
+            className="mt-1 w-full p-1 h-10 border-gray-400 border-2 bg-gray text-sm text-gray-700 shadow-sm"
           />
         </div>
 
