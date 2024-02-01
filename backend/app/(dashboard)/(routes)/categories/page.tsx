5605759a8a1e3eaff00c51ef9fc3e6981d2fbd94
Vaibhav Singh <vaibhavpratapsingh22@gmail.com>
2024-02-01 22:28:42 +0530

    parent cat

--- backend/app/(dashboard)/(routes)/categories/page.tsx
+++ backend/app/(dashboard)/(routes)/categories/page.tsx
@@ -1,21 +1,26 @@
 "use client";
 import axios from "axios";
+import { Delete, Edit } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import ReactSelect from "react-select";
 import { toast } from "react-toastify";
 
 const Categories = () => {
   const [catName, setCatName] = useState("");
   const [categories, setCategories] = useState([]);
+  const [parentCat, setParentCat] = useState("");
 
   const handleSaveCat = async () => {
     const data = {
       catName,
+      parentCat,
     };
     try {
       const response = await axios.post("/api/category", data);
       if (response.status === 201) {
         toast.success("Category saved successfully !");
         setCatName("");
+        getAllCategories();
       } else {
         toast.error(response.data.message);
       }
@@ -43,7 +48,7 @@ const Categories = () => {
   return (
     <>
       <h2>Create New Category </h2>
-      <div className="col-span-6 sm:col-span-3">
+      <div className="flex">
         <input
           type="text"
           id="Category Name"
@@ -52,7 +57,15 @@ const Categories = () => {
           required
           value={catName}
           onChange={(e) => setCatName(e.target.value)}
-          className="mt-1 w-[70%] px-5 h-10 border-gray-400 border-2 bg-gray text-sm text-gray-700 shadow-sm"
+          className="mt-1 mr-2 px-5 h-10 bg-gray text-sm text-gray-700 shadow-sm"
+        />
+        <ReactSelect
+          className="w-1/2 mt-1"
+          options={categories.map((cat: any) => ({
+            value: cat.id,
+            label: cat.name,
+          }))}
+          onChange={(e) => setParentCat(e?.value)}
         />
         <button
           className="bg-blue-500 hover:bg-blue-700 ml-2 text-white font-bold py-2 px-4 rounded"
@@ -61,42 +74,44 @@ const Categories = () => {
           Save
         </button>
       </div>
-      <table className="min-w-[80%] divide-y-2 divide-gray-200 bg-white text-sm mt-10">
-        <thead className="text-left">
-          <tr>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              Name
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {categories?.map((product: any) => (
-            <tr key={product.id}>
-              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                {product.name.toUpperCase()}
-              </td>
-              {/* <td className="whitespace-nowrap px-4 py-2 flex text-gray-700">
+      <div className="overflow-x-auto rounded-lg border border-gray-200 mt-10">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="text-left">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Name
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {categories?.map((product: any) => (
+              <tr key={product.id}>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  {product.name.toUpperCase()}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 flex text-gray-700">
                   <button
                     className="flex text-black font-bold mx-1 rounded"
-                    onClick={() => handleEditDelete("edit", product)}
+                    // onClick={() => handleEditDelete("edit", product)}
                   >
                     <Edit /> Edit
                   </button>
 
                   <button
                     className=" text-black flex justify-center items-center font-bold rounded"
-                    onClick={() => handleEditDelete("delete", product)}
+                    // onClick={() => handleEditDelete("delete", product)}
                   >
                     <Delete /> Delete
                   </button>
-                </td> */}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
--- backend/app/api/category/route.ts
+++ backend/app/api/category/route.ts
@@ -3,14 +3,26 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const { catName } = body;
+  const { catName, parentCat } = body;
   try {
-    const response = await prismadb.category.create({
-      data: {
-        name: catName,
-      },
-    });
-    if (response) return new NextResponse("Category created", { status: 201 });
+    if (parentCat) {
+      const response = await prismadb.category.create({
+        data: {
+          name: catName,
+          parentId: parentCat,
+        },
+      });
+      if (response)
+        return new NextResponse("Category created", { status: 201 });
+    } else {
+      const response = await prismadb.category.create({
+        data: {
+          name: catName,
+        },
+      });
+      if (response)
+        return new NextResponse("Category created", { status: 201 });
+    }
   } catch (err) {
     return new NextResponse("Error while creating category", { status: 400 });
   }
@@ -19,7 +31,7 @@ export async function POST(req: NextRequest) {
 export async function GET(req: NextRequest) {
   try {
     const response = await prismadb.category.findMany();
-    if (response) return  NextResponse.json(response, { status: 200 });
+    if (response) return NextResponse.json(response, { status: 200 });
   } catch (err) {
     return new NextResponse("Error while fetching categories", { status: 400 });
   }
